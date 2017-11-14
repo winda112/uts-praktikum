32ac79a2713675edaf6c28ab5610b450b1c0fe14
winda112 <33438342+winda112@users.noreply.github.com>
2017-11-14 07:49:12 +0700

    Add files via upload

--- konversi_uang.php
+++ konversi_uang.php
@@ -1,7 +1,19 @@
-<form method="get" action="konversi_uang.php">
+<html>
+<head>
+	<title> UTS PRAKTIKUM </title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<center>
+    <br></br>
+    <br></br>
+    <br></br>
+	<table>
+	<form action="konversi_uang.php" method="get" class="form-group">
+	<tr>
     Uang Rp. <input type="text" name="uang">
-    <input type="submit" value="Konversi" name="konversi">
-</form>
+    <input type="submit" value="Konversi" name="konversi"><br></br>
+   
 <?php
 if (isset($_GET['konversi'])){
     include "fungsi_terbilang.php";
@@ -11,6 +23,8 @@ if (isset($_GET['konversi'])){
     $terbilang = ucwords(terbilang($uang));
 
     echo "Uang Anda : Rp. $format_uang <br><br>";
-    echo "Terbilang : <b><br>$terbilang Rupiah</b>";
+    echo "Terbilang : $terbilang Rupiah</b>";
+    
 }
+
 ?>
